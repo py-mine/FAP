0d45c87ab08ccf828d9653b42ec78f7ec48e0ce6
Milo Weinberg <iapetus011@gmail.com>
2021-01-16 13:58:39 -0500

    move + rename defaults

--- fap/__init__.py
+++ fap/__init__.py
@@ -5,7 +5,8 @@ import git
 import os
 import re
 
-valid_url_regex = re.compile(
+DEFAULT = [['https://github.com/py-mine/FAP.git', 'FAP', 'fap']]
+VALID_URL_REGEX = re.compile(
     r'^(?:http)s?://'  # http:// or https://
     # domain...
     r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
@@ -20,12 +21,10 @@ loaded_plugins = []
 
 
 def dump_default():
-    default = [['https://github.com/py-mine/FAP.git', 'FAP', 'fap']]
-
     with open('plugins.yml', 'w+') as f:
-        f.write(yaml.dump(default))
+        f.write(yaml.dump(DEFAULT))
 
-    return default
+    return DEFAULT.copy()
 
 
 def load_plugin_list():
@@ -54,7 +53,7 @@ async def setup():
         plugins_dir.clone('https://github.com/py-mine/FAP.git')  # clone self to plugins directory
 
     for plugin_url, plugin_root, plugin_dir in load_plugin_list():
-        if re.match(valid_url_regex, plugin_url) is None:
+        if re.match(VALID_URL_REGEX, plugin_url) is None:
             raise ValueError(f'Entry in plugins.yml "{plugin}" is not a valid git clone/repository url.')
 
         if not os.path.isdir(plugin_root):
